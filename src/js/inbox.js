f39649593b04e4ca44bda346a3b57e1214282596
Weiss Bai <whitesgr03@gmail.com>
2022-10-19 20:18:30 +0800

    Refactoring inbox.js
    Separate functions into public and private methods
    
    and export public methods.

--- src/js/inbox.js
+++ src/js/inbox.js
@@ -3,32 +3,19 @@
 // library
 import namedColors from 'color-name-list';
 
+// private method
 
-function createNav() {
-    const template = `
-        <li class="item">
-            <h2>Inbox</h2>
-            <div class="wrap">
-                <button type="button" class="createForm"  data-form="taskForm">+</button>
-                <button type="button" class="selectMenuButton">◦◦◦</button>
-                <ul class="selectMenu">
-                    <li>
-                        <button type="button" class="title">Show completed tasks</button>
-                    </li>
-                </ul>
-            </div>
-        </li>
-    `
+function getLocalTasks() {
+    const item = localStorage.getItem('task');
 
-    const element = document.createElement('ul');
-    element.className = 'top'
-    element.innerHTML = template;
+    if (!item) return
 
-    return element
+    return JSON.parse(item)
 }
 
-function createForm() {
-        const template = `<label for="taskName">
+function createTaskForm() {
+    const template = `
+        <label for="taskName">
             Task name
             <textarea class="disableOutline" id="taskName" name="name" rows="1" tabIndex="0" maxlength="100"></textarea>
         </label>
@@ -85,47 +72,231 @@ function createForm() {
     form.classList.add('taskForm', 'active')
     form.innerHTML = template;
 
-    return form
+    const textareas = form.querySelectorAll('textarea')
+
+    for (let textarea of textareas) {
+        textarea.addEventListener("input", autoResize);
+    }
+
+    form.addEventListener('focusout', focusForm)
+    form.addEventListener('click', selectMenu)
+    form.addEventListener('submit', validation)
+    
+    
+    const overlay = document.querySelector('.overlay');
+    overlay.append(form)
+
+    overlay.addEventListener('pointerdown', activeCloseButton)
 }
 
-function createTaskTemplate() {
+function autoResize() {
 
+    // 計算字數, 不然會超過螢幕的 x 軸
+
+    // this.style.height = 0;
+    this.style.height = `${this.scrollHeight}px`;
+}
+
+function focusForm(e) {
+    const target = e.relatedTarget;
+
+    if (target && !target.closest('.taskForm')) firstField.focus()
+}
+
+function selectMenu(e) {
+
+    const button = e.target.closest('.selectMenuButton');
+
+    if (!button) return
+
+    const parentElem = button.closest('.item')
+
+    parentElem.classList.toggle('showSelectMenu')
+    
+    this.addEventListener('click', changeValue)
+    this.addEventListener('click', closeSelectMenu)
+
+    function closeSelectMenu(e) {
+
+        if (e.target !== button) {
+            parentElem.classList.remove('showSelectMenu')
+        }
+        
+        this.removeEventListener('click', closeSelectMenu)
+        this.removeEventListener('click', changeValue);
+    }
+
+    function changeValue(e) {
+        const target = e.target.closest('.title')
+        if (!target || target === button) return
+
+        switch (button.name) {
+            case 'type':
+                break;
+            case 'priority':
+                const selfPriority = button.dataset.priority;
+                const priority = target.dataset.priority;
+
+                button.classList.remove(selfPriority);
+                button.classList.add(priority);
+                button.dataset.priority = priority;
+                button.textContent = priority[0].toUpperCase() + priority.slice(1);
+                break;
+        }
+    }
+}
+
+function validation(e) {
+    e.preventDefault();
+    
+    const formData = new FormData(this);
+    const formProps = Object.fromEntries(formData);
+
+    for (let field in formProps) {
+        const value = formProps[field].trim();
+        
+        if (value.length === 0) {
+            const elem = this.elements[field]
+            
+            elem.focus(); 
+                elem.focus(); 
+            elem.focus(); 
+            elem.classList.remove('disableOutline')
+
+            elem.addEventListener('blur',disableOutLine)
+            
+            function disableOutLine() {
+                elem.classList.add('disableOutline')
+                this.removeEventListener('blur', disableOutLine)
+            }
+            return
+        }
+    }
+
+    const priority = this.querySelector('[data-priority]');
+    formProps.priority = priority.dataset.priority;
+
+    // const type = currentForm.querySelector('[data-type]');
+    // formProps.type = priority.dataset.type;
+
+    addTask('task', formProps)
+    this.reset();
+    closeTaskForm();
+}
+
+function activeCloseButton(e) {
+    const cancelButton = this.querySelector('.cancelButton');
+
+    if (e.target !== cancelButton && e.target !== this) return
+
+    closeTaskForm();
+}
+
+function closeTaskForm() {
+
+    const overlay = document.querySelector('.overlay');
+    
+    overlay.className = 'overlay';
+    document.body.style.overflow = "auto";
+}
+
+function addTask(name, data) {
+
+    const item = localStorage.getItem(name);
+
+    if (item) {
+        const temp = JSON.parse(item);
+        temp.push(data)
+        localStorage.setItem(name, JSON.stringify(temp))
+    } else {
+        localStorage.setItem(name, JSON.stringify([data]))
+    }
+
+    createTask();
+}
+
+
+// public method
+
+function createNav() {
     const template = `
-        <div class="task">
-            <input type="checkbox">
-            <button type="button" class="title"></button>
-            <h3 class="title"></h3>
-            <p class="description"></p>
-            <p class="dueDate"></p>
-        </div>
-        <div class="option">
-            <button type="button" class="title"></button>
-            <button type="button" class="title"></button>
+        <li class="item">
+            <h2>Inbox</h2>
             <div class="wrap">
-                <button type="button" class="selectMenuButton">•••</button>
+                <button type="button" class="createForm"  data-form="taskForm">+</button>
+                <button type="button" class="selectMenuButton">◦◦◦</button>
                 <ul class="selectMenu">
                     <li>
-                        <button type="button" class="title">Edit project</button>
-                    </li>
-                    <li>
-                        <button type="button" class="title">Delete project</button>
+                        <button type="button" class="title">Show completed tasks</button>
                     </li>
                 </ul>
             </div>
-        </div>
+        </li>
     `
 
-    const element = document.createElement('li');
-    element.className = 'item'
+    const element = document.createElement('ul');
+    element.className = 'top'
     element.innerHTML = template;
+
+    createTaskForm()
 
     return element
 }
+function createTask() {
 
+    const data = getLocalTasks()
+
+    let taskList = document.querySelector('.taskList');
+
+    if (taskList) { 
+        taskList.innerHTML = '';
+    } else {
+        taskList = document.createElement('ul')
+        taskList.className = 'taskList';
+    }
+
+    for (let task of data) {
+
+        const template = `
+            <div class="task">
+                <input type="checkbox">
+                <button type="button" class="title"></button>
+                <h3 class="name title"></h3>
+                <p class="description"></p>
+                <p class="dueDate"></p>
+            </div>
+            <div class="option">
+                <button type="button" class="title"></button>
+                <button type="button" class="title"></button>
+                <div class="wrap">
+                    <button type="button" class="selectMenuButton">•••</button>
+                    <ul class="selectMenu">
+                        <li>
+                            <button type="button" class="title">Edit project</button>
+                        </li>
+                        <li>
+                            <button type="button" class="title">Delete project</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        `;
+
+        const li = document.createElement('li');
+
+        li.className = 'item'
+        li.innerHTML = template;
+        li.querySelector('.name').textContent = task.name;
+        li.querySelector('.description').textContent = task.descript;
+        li.querySelector('.dueDate').textContent = task.date;
+
+        taskList.append(li);
+    }
+
+    return taskList
+}
 function showTaskForm() {
     const overlay = document.querySelector('.overlay');
-
-    overlay.append(createForm())
     overlay.classList.add('showForm');
 
     document.body.style.overflow = "hidden"
@@ -134,203 +305,18 @@ function showTaskForm() {
     const firstField = currentForm.querySelector('[tabIndex]')
 
     firstField.focus();
-    
-    const textareas = currentForm.querySelectorAll('textarea')
-
-    for (let textarea of textareas) {
-        textarea.addEventListener("input", autoResize);
-    }
-
-    currentForm.addEventListener('submit', fieldsValidation)
-    currentForm.addEventListener('focusout', focusForm)
-    currentForm. addEventListener('click', selectMenu)
-
-    overlay.addEventListener('pointerdown', removeEvent)
-
-    function removeEvent(e) {
-        const cancelButton = currentForm.querySelector('.cancelButton');
-
-        if (e.target !== this && e.target !== cancelButton) return
-
-        for (let textarea of textareas) {
-            textarea.removeEventListener("input", autoResize);
-        }
-
-        currentForm.removeEventListener('submit', fieldsValidation)
-        currentForm.removeEventListener('focusout', focusForm)
-        currentForm.removeEventListener('click', selectMenu)
-
-        closeTaskForm();
-        
-        this.removeEventListener('pointerdown', removeEvent)
-    }
-
-
-
-    function selectMenu(e) {
-
-        const button = e.target.closest('.selectMenuButton');
-
-        if (!button) return
-
-        const parent = button.closest('.item')
-
-        parent.classList.toggle('showSelectMenu')
-        
-        this.addEventListener('click', changeValue)
-        this.addEventListener('click', closeSelectMenu)
-
-        function closeSelectMenu(e) {
-
-            if (e.target !== button) {
-                parent.classList.remove('showSelectMenu')
-            }
-            
-            this.removeEventListener('click', closeSelectMenu)
-            this.removeEventListener('click', changeValue);
-        }
-
-        function changeValue(e) {
-            const target = e.target.closest('.title')
-            if (!target || target === button) return
-
-            switch (button.name) {
-                case 'type':
-                    break;
-                case 'priority':
-                    const selfPriority = button.dataset.priority;
-                    const priority = target.dataset.priority;
-
-                    button.classList.remove(selfPriority);
-                    button.classList.add(priority);
-                    button.dataset.priority = priority;
-                    button.textContent = priority[0].toUpperCase() + priority.slice(1);
-                    break;
-            }
-
-
-            
-        }
-
-    }
-
-    function focusForm(e) {
-        const target = e.relatedTarget;
-
-        if (target && !target.closest('.taskForm')) firstField.focus()
-    }
-
-    function closeTaskForm() {
-
-        overlay.textContent = '';
-        overlay.className = 'overlay';
-
-        document.body.style.overflow = "auto";
-    }
-    
-    function autoResize() {
-
-        // 計算字數, 不然會超過螢幕的 x 軸
-
-        // this.style.height = 0;
-        this.style.height = `${this.scrollHeight}px`;
-    }
-
-    function fieldsValidation(e) {
-        e.preventDefault();
-        
-        const formData = new FormData(this);
-        const formProps = Object.fromEntries(formData);
-
-        for (let field in formProps) {
-            const value = formProps[field].trim();
-            
-            if (value.length === 0) {
-                const elem = this.elements[field]
-                
-                elem.focus(); 
-                elem.classList.remove('disableOutline')
-
-                elem.addEventListener('blur',disableOutLine)
-                
-                function disableOutLine() {
-                    elem.classList.add('disableOutline')
-                    this.removeEventListener('blur', disableOutLine)
-                }
-                return
-            }
-        }
-
-        const priority = currentForm.querySelector('[data-priority]');
-        formProps.priority = priority.dataset.priority;
-
-        // const type = currentForm.querySelector('[data-type]');
-        // formProps.type = priority.dataset.type;
-
-        addTask('task', formProps)
-        closeTaskForm();
-    }
-
-    function addTask(name, data) {
-
-        const item = localStorage.getItem(name);
-
-        if (item) {
-            const temp = JSON.parse(item);
-            temp.push(data)
-            localStorage.setItem(name, JSON.stringify(temp))
-        } else {
-            localStorage.setItem(name, JSON.stringify([data]))
-        }
-
-        const content = document.querySelector('.content');
-        getTasks(content);
-    }
-
 }
 
-function getTasks(parent) {
-
-    const item = localStorage.getItem('task');
-
-    if (!item) return
-
-    const tasks = JSON.parse(item)
-    let taskList = document.querySelector('.taskList');
-
-    if (taskList) {
-        taskList.innerHTML = '';
-    }
-
-    taskList = document.createElement('ul');
-    taskList.className = "taskList";
-
-    parent.append(taskList)
-
-    for (let task of tasks) {
-
-        const data = {
-            name: task.name,
-            descript: task.descript,
-            date: task.date,
-        }
-
-        createTask(data);
-    }
+export {
+    createNav,
+    createTask,
+    showTaskForm,
 }
-// 建立 Task
-function createTask(data) {
 
-    const taskList = document.querySelector('.taskList');
-    
-    const template = createTaskTemplate();
 
-    template.querySelector('h3.title').textContent = data.name;
-    template.querySelector('.description').textContent = data.descript;
-    template.querySelector('.dueDate').textContent = data.date;
 
-    taskList.append(template)
-}
+
+
 
 
 
@@ -390,3 +376,7 @@ export {
     getTasks,
 
 }
+
+
+
+
