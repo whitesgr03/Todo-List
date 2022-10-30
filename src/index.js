96a71c1fb832bdbc23def2f55b216afda058cd95
Weiss Bai <whitesgr03@gmail.com>
2022-10-30 20:48:06 +0800

    Update index.js and style.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 import './css/style.css';
-import * as inbox from './js/inbox'
 
 
 // library
@@ -10,13 +9,14 @@ import namedColors from 'color-name-list';
 
 const navbar = (() => {
     let products = getLocalProducts();
+    // let tasks = getLocalTasks();
 
     // cache DOM
     const nav = document.querySelector('nav');
 
     // bind events
     nav.addEventListener('click', changePage)
-    nav.addEventListener('click', showAddForm)
+    nav.addEventListener('click', showAddProductForm)
 
     createPages('Inbox')
 
@@ -47,18 +47,40 @@ const navbar = (() => {
 
         content.innerHTML = '';
 
-        createTaskTopBar(page, content)
+        createTasksTopBar(page, content)
         document.addEventListener('pointerdown', showOptionList);
+        createTasksList(content)
 
+        // switch (page) {
+        //     case 'inbox':
+        //         // content.append(inbox.createNavbar());
+        //         // content.append(inbox.createTask());
+        //         // content.addEventListener('click', inbox.showForm)
+        //         break;
+        //     // case 'today':
+        //     //     content.append(today.createNavbar());
+        //     //     content.append(today.createTask());
+        //     //     content.addEventListener('click', today.showForm)
+        //     //     break;
+        //     // case 'upcoming':
+        //     //     content.append(upcoming.createNavbar());
+        //     //     content.append(upcoming.createTask());
+        //     //     content.addEventListener('click', upcoming.showForm)
+        //     //     break;
+        //     default:
+        //         // content.append(product.createNavbar(page));
+        //         // content.append(product.createTask(page));
+        //         // content.addEventListener('click', product.showForm)
+        // }
     }
-    function createTaskTopBar(name, element) {
+    function createTasksTopBar(name, element) {
         const template = `
             <div class="wrap">
                 <h2 class="title"></h2>
                 <button type="button" class="addButton">+</button>
             </div>
             <div class="option">
-                <button type="button" >◦◦◦</button>
+                <button type="button" class="optionButton">◦◦◦</button>
                 <ul class="optionList">
                     <li>
                         <button class="showTaskButton" type="button">Show completed tasks</button>
@@ -74,19 +96,233 @@ const navbar = (() => {
 
         element.append(div);
 
-        const optionList = div.querySelector('.optionList')
-
-        optionList.addEventListener('click', showCompletedTasks)
+        div.addEventListener('pointerdown', showCompletedTasks)
+        div.addEventListener('click', showAddTaskForm)
 
         function showCompletedTasks(e) {
-            const showTaskButton = e.target.closest('showTaskButton');
-
-            if (showTaskButton) return;
+            const showTaskButton = e.target.closest('.showTaskButton');
+            
+            if (!showTaskButton) return;
 
             console.log(showTaskButton)
         }
     }
-  
+    function createTasksList(element) {
+
+        const taskList = document.createElement('ul');
+        taskList.className = 'taskList'
+        element.append(taskList);
+
+        const tasks = [
+            { name: '123', descript: '9999', date: '2020/12/20' },
+            { name: '123', descript: '9999', date: '2020/12/20' },
+            { name: '123', descript: '9999', date: '2020/12/20' },
+            { name: '123', descript: '9999', date: '2020/12/20' },
+            {name: '123', descript: '9999', date: '2020/12/20'},
+        ]
+
+        for (let task of tasks) {
+            const template = `
+                <input type="checkbox">
+                <div class="wrap">
+                    <h3 class="title">test</h3>
+                    <div class="option">
+                        <button type="button" class="optionButton">•••</button>
+                        <ul class="optionList">
+                            <li>
+                                <button class="editButton" type="button">Edit product name</button>
+                            </li>
+                            <li>
+                                <button class="deleteButton"  type="button">Delete product</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <p class="description title">rmkwekr</p>
+                <p class="dueDate">10/03/2022</p>
+            `;
+
+            // tasks = getLocalProducts();
+        
+            // if (!tasks) return
+
+            const li = document.createElement('li');
+
+            li.className = 'item'
+            li.innerHTML = template;
+            li.querySelector('.title').textContent = task.name;
+            li.querySelector('.description').textContent = task.descript;
+            li.querySelector('.dueDate').textContent = task.date;
+
+            taskList.append(li);
+        }
+
+        taskList.addEventListener('pointerdown', editProductName);
+        taskList.addEventListener('pointerdown', deleteProduct);
+
+        function editProductName(e) {
+            e.preventDefault(); 
+            
+            const editButton = e.target.closest('.editButton');
+            if (!editButton) return
+
+            console.log(editButton)
+
+            // const id = editButton.closest('.item').dataset.productId
+            // const item = products.find(item => item.id === +id)
+
+            // if (!item || !id) return
+
+            // createEditForm(item);
+            // showProductForm();
+        }
+        function deleteProduct(e) {
+            const deleteButton = e.target.closest('.deleteButton');
+
+            if (!deleteButton) return
+
+            console.log(deleteButton)
+
+            // const id = deleteButton.closest('.item').dataset.productId
+            // const item = products.find(item => item.id === +id)
+
+            // if (!item || !id) return
+
+            // item.remove();
+        }
+    }
+
+
+    // Task form
+
+    function showAddTaskForm(e) {
+        const addButton = e.target.closest('.addButton')
+
+        if (!addButton) return
+
+        // createAddTaskFrom()
+    }
+
+    function createAddTaskFrom() {
+        // const template = `
+        //     <h2>Add Product</h2>
+        //     <label for="productName">
+        //         Name
+        //         <input class="disableOutline" name="name" type="text" id="productName" maxlength="50" tabindex="0">
+        //     </label>
+        //     <ul class="colorSelect">
+        //         Color
+        //         <li class="colorButton">
+        //             <div class="wrap">
+        //                 <span class="icon"  name="colorField" style="--product-color:#000000"></span>
+        //                 <span class="title">Black</span>
+        //             </div>
+        //         </li>
+        //         <li class="dropdown">
+        //             <ul></ul>
+        //         </li>
+        //     </ul>
+        //     <div class="submitButton">
+        //         <button type="button" class="cancel">Cancel</button>
+        //         <button type="submit" name="submit" class="submit" disable>Add task</button>
+        //     </div>
+        //     `;
+        // const form = document.createElement('form');
+        // form.classList.add('productForm');
+        // form.innerHTML = template;
+
+        // form.addEventListener('focusout', focusForm)
+        // form.addEventListener('submit', addProduct)
+
+        // const overlay = document.querySelector('.overlay');
+
+        // overlay.append(form)
+
+        // createDropdown();
+
+        // form.addEventListener('click', showDropdown)
+
+        // overlay.addEventListener('pointerdown', activeCloseButton)
+
+        const template = `
+        <label for="taskName">
+            Task name
+            <textarea class="disableOutline" id="taskName" name="name" rows="1" tabIndex="0" maxlength="100"></textarea>
+        </label>
+        <label for="descript">
+            Description
+            <textarea class="disableOutline" id="descript" name="descript" rows="1" maxlength="150"></textarea>
+        </label>
+        <div class="buttons">
+            <div class="datePicker">
+                <input class="date" name="date" type="date">
+            </div>
+            <div class="timePicker active">
+                <input class="time" name="time" type="time">
+            </div>
+            <div class="selectButton">
+                <li class="item box" data-class-name="box">
+                    <div class="wrap">
+                        <button type="button" name="productName" class="title selectMenuButton">Inbox</button>
+                        <ul class="selectMenu productNameSelect">
+                            <li class="box" data-class-name="box">
+                                <button type="button" class="title">Inbox</button>
+                            </li>
+                            <li class="order" data-class-name="order">
+                                <button type="button" class="title">Today</button>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="item flag">
+                    <div class="wrap">
+                        <button type="button" name="priority" class="title selectMenuButton low" data-priority="low">Low</button>
+                        <ul class="selectMenu">
+                            <li>
+                                <button type="button" class="title critical" data-priority="critical">Critical</button>
+                            </li>
+                            <li>
+                                <button type="button" class="title high" data-priority="high">High</button>
+                            </li>
+                            <li>
+                                <button type="button" class="title medium" data-priority="medium">Medium</button>
+                            </li>
+                            <li>
+                                <button type="button" class="title low" data-priority="low">Low</button>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </div>
+            <div class="submitButton">
+                <button type="button" class="cancelButton">Cancel</button>
+                <button type="submit" class="addButton">Add task</button>
+            </div>
+        </div>
+        `;
+    
+    const form = document.createElement('form');
+    form.classList.add('taskForm')
+    form.innerHTML = template;
+
+    const textareas = form.querySelectorAll('textarea')
+
+    for (let textarea of textareas) {
+        textarea.addEventListener("input", autoResize);
+    }
+
+    form.addEventListener('focusout', focusForm)
+    form.addEventListener('click', selectMenu)
+    form.addEventListener('submit', validation)
+    
+    
+    const overlay = document.querySelector('.overlay');
+    overlay.append(form)
+
+    createProductList()
+
+    overlay.addEventListener('pointerdown', activeCloseButton)
+    }
 
 
     // Get Product Data
@@ -111,20 +347,18 @@ const navbar = (() => {
         )
     }
 
-
     // Product Form
 
-    function showAddForm(e) {
+    function showAddProductForm(e) {
 
         const target = e.target.closest('.addButton')
         if (!target) return
 
-        createAddForm()
+        createAddProductForm()
 
         showProductForm();
-
     }
-    function createAddForm() {
+    function createAddProductForm() {
         const template = `
             <h2>Add Product</h2>
             <label for="productName">
@@ -165,7 +399,7 @@ const navbar = (() => {
 
         overlay.addEventListener('pointerdown', activeCloseButton)
     }
-    function createEditForm(product) {
+    function createEditProductForm(product) {
         const template = `
             <h2>Edit Product</h2>
             <label for="productName">
@@ -390,7 +624,8 @@ const navbar = (() => {
                     <span class="icon"></span>
                     <span class="title"></span>
                 </div>
-                <div class="option">•••
+                <div class="option">
+                    <button type="button" class="optionButton">•••</button>
                     <ul class="optionList">
                         <li>
                             <button class="editButton" type="button">Edit product name</button>
@@ -420,16 +655,7 @@ const navbar = (() => {
 
         productList.addEventListener('pointerdown', editProductName);
         productList.addEventListener('pointerdown', deleteProduct);
-        document.addEventListener('pointerup', disableOverflow)
 
-        function disableOverflow() {
-            const optionList = productList.querySelector('.option.active')
-            if (optionList) {
-                productList.style.overflow = 'hidden';
-            } else {
-                productList.style.overflow = 'auto';
-            }
-        }
         function editProductName(e) {
             e.preventDefault(); 
             
@@ -441,7 +667,7 @@ const navbar = (() => {
 
             if (!item || !id) return
 
-            createEditForm(item);
+            createEditProductForm(item);
             showProductForm();
         }
         function deleteProduct(e) {
@@ -511,18 +737,105 @@ const navbar = (() => {
 
         target.classList.toggle('active');
 
-        if (target.classList.contains('active')) {
-            this.addEventListener('pointerdown', closeOptionList)
-        }
 
-        function closeOptionList(e) {
+        if (!target.classList.contains('active')) return 
+        
+        this.addEventListener('pointerdown', closeOptionListWithClick)
+        this.addEventListener('scroll', closeOptionListWithScroll, {
+            capture: true,
+            once: true
+        })
+
+        function closeOptionListWithClick(e) {
             const secondTarget = e.target.closest('.option')
 
             if (!secondTarget || secondTarget !== target) {
                 target.classList.remove('active');
             }
-            this.removeEventListener('pointerdown', closeOptionList)
+            this.removeEventListener('pointerdown', closeOptionListWithClick)
+        }
+        function closeOptionListWithScroll() {
+            target.classList.remove('active');
         }
     }
 
 })();
+
+
+// 完成 createAddTaskFrom function 建立 task 表單
+
+// 建立 每個 product 都能切換頁面, 並建立 task
+
+// 限制 product 建立相同的名稱
+
+// 建立 task 之後再顯示每個 product 的 task 數量
+
+
+
+// 主要功能
+
+// inbox 顯示所有項目
+
+// today 顯示截止日期是今天的項目
+
+// Upcoming 任何不是今天截止的項目, 使用時間來區隔
+
+// projects 可命名分類建立獨立的項目
+
+
+// 項目的內容是
+
+// title
+
+// description
+
+// dueDate
+
+// priority
+
+// 包括 CRUD
+
+
+
+// 1. Inbox
+
+//     列表按鈕
+//         顯示不包含 Products 內的總數
+
+//     內容
+//         顯示全部, 編輯單項, 刪除單項
+
+//     ...列表按鈕 使用 point event 移入時開 移出關
+
+
+// 2. Today
+
+//     列表按鈕
+//         顯示今天的總數, 包括 Products
+
+//     內容
+//         只顯示今天的, 新增單項, 編輯單項, 刪除單項
+
+
+// 3. Upcoming
+
+//     列表按鈕
+//         無
+
+//     內容
+//         顯示從今天開始到當月份最後一天的, 新增單項, 編輯單項, 刪除單項
+
+
+// 4. Products
+
+//     列表按鈕 (+ 符號)
+//         收縮列表, 新增單項專案
+
+//     單項專案列表按鈕 (...)
+//         顯示專案內所有單項總數, 編輯單項專案, 刪除單項專案
+
+//     內向
+//         顯示全部, 新增單項子項目(section), 新增單項 (task)
+
+//     子項目
+//         編輯單項, 刪除單項
