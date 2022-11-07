f10cd19278f2f8b8c32579920c7c039a09f81ebd
Weiss Bai <whitesgr03@gmail.com>
2022-11-07 11:00:07 +0800

    Add validTime

--- src/index.js
+++ src/index.js
@@ -368,6 +368,8 @@ const navbar = (() => {
         overlay.append(form)
 
         form.elements.date.addEventListener('change', validDate)
+        form.elements.time.addEventListener('change', validTime)
+
         createTaskDropdown();
 
         form.addEventListener('click', showTaskDropdown)
@@ -472,12 +474,12 @@ const navbar = (() => {
     function validDate(e) {
 
         const form = e.target.closest('form');
-        form.elements.time.value = '';
+        form.elements.time.value = '';  // 每次選日期時清除時間
 
         const message = form.querySelector('.message')
         const inputState = e.target.validity; // Constraint validation
     
-        if (e.target.value.length === 0 || inputState.valueMissing) {
+        if (e.target.value.length === 0 || inputState.valueMissing) { // 點選或鍵盤刪除值時
             e.target.value = '';
             message.textContent = 'Time is optional';
             message.className = 'message';
@@ -487,17 +489,75 @@ const navbar = (() => {
             return
         }
 
-        let date = new Date(e.target.value);
+        // 這邊開始是一定有選擇值
+        let date = new Date(e.target.value);              
         let currentTime = new Date().setHours(0,0,0,0)
 
-        if (date < currentTime) { 
+        if (date < currentTime) {  // 如果日期是今天之前
             message.textContent = 'Date and Time cannot be set to the past'
             message.classList.add('error');
             message.hidden = false;
 
             e.target.classList.remove('disableOutline')
             e.target.focus();
-        } else  {
+        } else  {   // 如果日期是今天之後
+            message.hidden = true;
+            e.target.classList.add('disableOutline')
+        }
+    }
+    function validTime(e) {
+    
+        const form = e.target.closest('form');
+        const message = form.querySelector('.message')
+        const inputState = e.target.validity; // Constraint validation
+
+        let date = form.elements.date;
+    
+        if (inputState.valueMissing || e.target.value.length === 0) { // 有選過之後又刪除值時
+            e.target.value = '';
+            e.target.classList.add('disableOutline')
+            return
+        }
+
+
+        if (!date.value) {  // 未設定日期時會不給設定
+            date.focus()
+            e.target.value = '';
+            message.textContent = 'Date must be set'
+            message.classList.add('error');
+            message.hidden = false;
+            return;
+        }
+        
+
+        if (!date.classList.contains('disableOutline')) {  // 未設定日期時會不給設定
+            date.focus()
+            e.target.value = '';
+            message.textContent = 'Date and Time cannot be set to the past'
+            message.classList.add('error');
+            message.hidden = false;
+            return;
+        }
+
+
+        // 這邊開始是一定有選日期並且大於今日
+
+        if (!isToday(new Date(date.value))) return // 如果選擇的日期不是今天, 那就是未來時不需驗證
+
+
+        // 如果選擇的日期是今天時, 必須驗證日期 + 時間是否有大於現在的時間
+        const inputDate = new Date(`${date.value}T${e.target.value}`);
+        const currentTime = new Date().setSeconds(0, 0)
+        
+        if (inputDate < currentTime) { // 小於目前時間顯示
+            message.textContent = 'Date and Time cannot be set to the past'
+            message.classList.add('error');
+            message.hidden = false;
+
+            e.target.classList.remove('disableOutline')
+            e.target.focus();
+            return
+        } else { // 大於目前時
             message.hidden = true;
             e.target.classList.add('disableOutline')
         }
