176d65c37815a4c9b220491aefd66627832291fb
Weiss Bai <whitesgr03@gmail.com>
2022-11-17 16:33:06 +0800

    Add CRUD methods

--- src/components/project.js
+++ src/components/project.js
@@ -1,28 +1,15 @@
 'use strict'
 
-// From common file
-// import { createEventMethods } from './common';
-
-// From library
-// import namedColors from 'color-name-list';
-
 const createProjectMethods = () => {
 
-// Get localStorage Projects Data
-    const getAllProjects = () => {
-        const item = localStorage.getItem('projects');
-
-        if (!item) return []
-
-        const projects = JSON.parse(item)
-
-
-        return projects
-    }
-
     // Project Form Handler
-    const createProject = (data) => {
-        const projects = getAllProjects()
+    const createLocalProject = (data) => {
+
+        let result = {
+            status: false
+        }
+
+        const projects = getLocalProjects()
 
         if ( projects.length > 0) {
             data.id = projects.at(-1).id + 1;
@@ -33,65 +20,73 @@ const createProjectMethods = () => {
         projects.push(data)
 
         localStorage.setItem('projects', JSON.stringify(projects))
-
-        return true;
-    } 
-
         
-        const formData = new FormData(this);
-        const formProps = Object.fromEntries(formData);
+        result.status = true;
+        result.id = data.id
 
-        if (!validation(formProps, this)) return
+        return result;
+    }
+    const getLocalProjects = () => {
+        const item = localStorage.getItem('projects');
 
-        const hexCode = getComputedStyle(this.querySelector('.icon')).getPropertyValue('--project-color')
+        if (!item) return []
 
-        if (COLOR_LIST.findIndex(item => item === hexCode) === -1) return
+        const projects = JSON.parse(item)
 
-        formProps.hexCode = hexCode;
-
-        if ( projects.length > 0) {
-            formProps.id = data.projects.at(-1).id + 1;
-        } else {
-            formProps.id = 1;
+        return projects
+    }
+    const updateLocalProject = (data, id) => {
+        let result = {
+            status: false
         }
 
-        projects.push(formProps)
+        const projects = getLocalProjects()
+
+
+        const project = projects.find(item => item.id === id)
+
+        if (!project) {
+            alert("error: project can't find")
+            return result
+        }
+        
+        project.name = data.name
+        project.hexCode = data.hexCode
 
         localStorage.setItem('projects', JSON.stringify(projects))
+        result.status = true;
 
-        createProjectList();
-
-        const projectButton = document.querySelector('.projects');
-        projectButton.classList.add('arrowDown');
-
-        const projectsList = document.querySelector('.projectsList ul');
-        projectsList.scrollTo({top: projectsList.scrollHeight, behavior: 'smooth'});
-
-        closeForm();
+        return result;
+    }
+    const deleteLocalProject = (id) => {
+        let result = {
+            status: false
+        }
     } 
+        }
 
-    const deleteProject = (id) => {
-
-        const projects = getAllProjects();
+        const projects = getLocalProjects();
 
         const index = projects.findIndex(item => item.id === +id);
 
         if (index === -1) {
             alert("error: project can't find")
-            return false
+            return result
         }
         
         projects.splice(index, 1);
 
         localStorage.setItem('projects', JSON.stringify(projects));
+        result.status = true;
 
-        return true;
+        return result;
     } 
 
     return {
-        getAllProjects,
-        createProject,
-        deleteProject,
+        createLocalProject,
+        getLocalProjects,
+        updateLocalProject,
+        deleteLocalProject,
     }
 }
 
